832bd66973de24848bdc593f75d8cde67c40c9b5
Baril <barh09@uqat.ca>
2026-04-13 14:40:25 -0400

    Ajout des dossiers pour les monstres

---
+++ Ennemies.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Character.h"
+#include "Player.h"
+
+/*----------------------------------*/
+//                                  //
+// MADE BY HENRICK BARIL 2026-04-09 //
+//                                  //
+/*----------------------------------*/
+
+class Ennemies : public Character
+{
+public:
+    void Draw(AcademiaEngine& engine) override;
+    void AddForce(AcademiaEngine& engine, float force, std::vector<float> direction);
+    olc::vf2d GetPlayerPosition(AcademiaEngine& engine, Player player);
+    void GoToPlayer(olc::vf2d playerPosition);
+
+    float Radius = 20.0f;
+    olc::Pixel Color = olc::WHITE;
+};
