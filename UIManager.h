2a5f83d71b5d483f10f6b84b9310f5147a9c3c6d
Henrick Baril <henrick.baril@pikogan.com>
2026-06-05 11:21:42 -0400

    UI improvement

--- UIManager.h
+++ UIManager.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "UIElement.h"
+#include <vector>
+
+class AcademiaEngine;
+
+class UIManager
+{
+public:
+	void Initialize(AcademiaEngine* engineContext);
+	void Update(float dt);
+	void Render();
+	olc::vf2d GetMousePosition(AcademiaEngine& engine) const;
+
+protected:
+	std::vector<std::unique_ptr<UIElement>> _elements;
+
+private:
+	AcademiaEngine* _EngineContext = nullptr;
+
+};
